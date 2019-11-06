535cee0fb11cb1bf4a36cef4a1fa05a03728d84d
vladislav1133 <vladislav1133@ukr.net>
2019-11-06 18:33:45 +0200

    install morgan (test-middleware) & install mongoose

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const logger = require('./middlewares/logger');
+const morgan = require('morgan');
 
 // Route files
 const bootcamps = require('./routes/bootcamp');
@@ -9,6 +11,11 @@ dotenv.config({ path: './config/config.env'});
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+// Middleware
+if(process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'));
+}
 
 // Mount routes
 app.use('/api/v1/bootcamps', bootcamps);
