cf8c4ca384b2f2b0030b71bbd3bdf6faf240282b
vladislav1133 <vladislav1133light@ukr.net>
2019-11-07 21:59:31 +0200

    bootcamp controller: get & update method

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -3,15 +3,28 @@ const Bootcamp = require('../models/Bootcamp');
 // @desc    Get all bootcmaps
 // @route   GET /api/v1/bootcamps
 // @access  Public
-exports.getBootcamps = (req, res, next) => {
-    res.status(200).json({ success: true, msg: 'show all bootcamps'});
+exports.getBootcamps = async (req, res, next) => {
+    try {
+        const bootcamps = await Bootcamp.find();
+        res.status(200).json({ success: true, data: bootcamps });
+    } catch (err) {
+        res.status(400).json({ success: false});
+    }
 };
 
 // @desc    Get single bootcmap
 // @route   GET /api/v1/bootcamps/:id
 // @access  Public
-exports.getBootcamp = (req, res, next) => {
-    res.status(200).json({ success: true, msg: `show bootcamp #${req.params.id}`});
+exports.getBootcamp = async (req, res, next) => {
+    try {
+        const bootcamp = await Bootcamp.findById(req.params.id);
+
+        if(!bootcamp) return res.status(400).json({ success: false});
+
+        res.status(200).json({ success: true, data: bootcamp });
+    } catch (err) {
+        res.status(400).json({ success: false});
+    }
 };
 
 // @desc    Create new bootcmap
@@ -26,7 +39,7 @@ exports.createBootcamp = async (req, res, next) => {
             data: bootcamp
         });
     } catch (err) {
-        res.status(400).json({ status: false })
+        res.status(400).json({ status: false });
     }
 
 };
@@ -34,8 +47,15 @@ exports.createBootcamp = async (req, res, next) => {
 // @desc    Update bootcmap
 // @route   PUT /api/v1/bootcamps/:id
 // @access  Private
-exports.updateBootcamp = (req, res, next) => {
-    res.status(200).json({ success: true, msg: `update bootcamp #${req.params.id}`});
+exports.updateBootcamp = async (req, res, next) => {
+    const bootcamp = await Bootcamp.findByIdAndUpdate(req.params.id, req.body, {
+        new: true,
+        runValidators: true
+    });
+
+    if(!bootcamp) return res.status(400).json({ success: false});
+
+    res.status(200).json({ success: true, data: bootcamp });
 };
 
 // @desc    Delete bootcmap
